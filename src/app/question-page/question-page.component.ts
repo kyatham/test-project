acd26e4db8e31ca46ba2b332c5fb2e0bcef5ff81
“ARJUN” <TENNIS.ARJUN@GMAIL.COM>
2019-07-06 23:27:17 +0530

    minor fixes

--- src/app/question-page/question-page.component.ts
+++ src/app/question-page/question-page.component.ts
@@ -3,8 +3,10 @@
   It is upto us to make this more extensible as needed by the user
 */
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { QuestionsService } from '../questions.service';
+
 
 @Component({
   selector: 'app-question-page',
@@ -13,14 +15,35 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class QuestionPageComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private questionsService: QuestionsService) {
   }
 
   id: string;
 
+  question: any;
+
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.id = params.id;
+      this.getQuestion();
+    });
+  }
+
+  getQuestion() {
+    this.questionsService.getQuestions()
+      .subscribe((questions: any) => {
+        this.question = questions.feed_questions.find((question) => { // Get the  question selected in context
+          return question.Id === this.id;
+        });
+        this.questionsService.getAnswers()
+          .subscribe((answers: any) => {
+            const allAnswers = answers.feed_answers.sort((a, b) => { // Sorting by most recent timestamp first
+              return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+            });
+            this.question.answers = allAnswers.filter((answer) => {
+              return answer['Question-Id'] === this.question.Id; //  Finding answers for current question in context
+            });
+          });
     });
   }
 
